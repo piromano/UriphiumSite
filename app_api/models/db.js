4fa3ce921c88c40a2e425a333368e158bb4b7d4b
alexander sun <luc.alexander.sun@icloud.com>
2016-03-23 17:17:36 +0800

    add server code

---
+++ app_api/models/db.js
@@ -0,0 +1,47 @@
+
+var mongoose = require( 'mongoose' );
+var dbURI = 'mongodb://192.168.99.100:32768/Uriphium';
+if (process.env.NODE_ENV === 'production') {
+  dbURI = 'mongodb://heroku_app20110907:4rqhlidfdqq6vgdi06c15jrlpf@ds033669.mongolab.com:33669/heroku_app20110907';
+}
+
+mongoose.connect(dbURI);
+
+
+var dbURIlog = 'mongodb://192.168.99.100:32768/UriphiumLog';
+var logDB = mongoose.createConnection(dbURIlog);
+
+mongoose.connection.on('connected', function () {
+  console.log('Mongoose connected to ' + dbURI);
+});
+mongoose.connection.on('error',function (err) {
+  console.log('Mongoose connection error: ' + err);
+});
+mongoose.connection.on('disconnected', function () {
+  console.log('Mongoose disconnected');
+});
+
+var gracefulShutdown = function (msg, callback) {
+  mongoose.connection.close(function () {
+    console.log('Mongoose disconnected through ' + msg);
+    callback();
+  });
+};
+
+process.once('SIGUSR2', function () {
+  gracefulShutdown('nodemon restart', function () {
+    process.kill(process.pid, 'SIGUSR2');
+  });
+});
+process.on('SIGINT', function () {
+  gracefulShutdown('app termination', function () {
+    process.exit(0);
+  });
+});
+process.on('SIGTERM', function() {
+  gracefulShutdown('Heroku app shutdown', function () {
+    process.exit(0);
+  });
+});
+// BRING IN YOUR SCHEMAS & MODELS
+require('./locations');
--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,33 +1,68 @@
+var request = require('request');
+var apiOptions = {
+  server : "http://localhost:3000"
+}
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://getting-mean-loc8r.herokuapp.com";
+}
 /* GET 'home' page */
-module.exports.homelist = function(req, res){ 
-	res.render('locations-list', { title: 'Uriphium - Where geeks meet!',
+var renderHomepage = function(req, res,responseBody){
+	var message;
+	if (!(responseBody instanceof Array)) {
+	 	message = "API lookup error";
+	 	responseBody = [];
+	} else {
+	 	if (!responseBody.length) {
+			message = "No places found nearby";
+		} 
+	}
+    res.render('locations-list', { 
+    	title: 'Uriphium - Where geeks meet!',
 		pageHeader:{
 			title: 'Uriphium',
 			strapline:'Meet Startups'
 		},
-		locations: [{
-		      name: 'Starcups',
-		      address: '125 High Street, Reading, RG6 1PS',
-		      rating: 3,
-		      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-		      distance: '100m'
-		    },{
-		      name: 'Cafe Hero',
-		      address: '125 High Street, Reading, RG6 1PS',
-		      rating: 4,
-		      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
-		      distance: '200m'
-		    },{
-		      name: 'Burger Queen',
-		      address: '125 High Street, Reading, RG6 1PS',
-		      rating: 2,
-		      facilities: ['Food', 'Premium wifi'],
-		      distance: '250m'
-		    }],
-		    sidebar: "Looking for wifi and a seat? Uriphium helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
+		locations: responseBody,
+		message: message,
+		sidebar: "Looking for wifi and a seat? Uriphium helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for."
 	});
 };
-
+module.exports.homelist = function(req, res){ 
+	var requestOptions, path;
+	path = '/api/locations';
+	requestOptions = {
+		url : apiOptions.server + path,
+		method : "GET",
+		json : {},
+		qs : {
+			lng : -0.8690884,
+			lat : 51.435041,
+			maxDistance : 20
+		} };
+		request(
+			requestOptions,
+			function(err, response, body) {
+				var i, data;
+    			data = body;
+    			if (response.statusCode === 200 && data.length){
+    				for (i=0; i<data.length; i++) {
+  						data[i].distance = _formatDistance(data[i].distance);
+  					}
+    			}
+				renderHomepage(req, res, data);
+			}
+			); 
+};
+var _formatDistance = function (distance) {
+  var numDistance, unit;
+  if (distance > 1) {
+    numDistance = parseFloat(distance).toFixed(1);
+    unit = 'km';
+  } else {
+    numDistance = parseInt(distance * 1000,10);
+unit = 'm'; }
+  return numDistance + unit;
+};
 
 module.exports.locationInfo = function(req, res){
 	res.render('location-info', {
